4d2009202a05bf163d23fa4967e71f3798bf4b11
darksr789 <dyt27380@gmail.com>
2026-05-01 00:50:12 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,48 +1,53 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router()
-const pino = require("pino");
+let router = express.Router();
+const pino = require('pino');
 const {
-    default: Maher_Zubair,
+    default: DARK_SURYA,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require('@whiskeysockets/baileys');
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function SURYA_X_PAIR_CODE() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/'+id)
-     try {
-            let Pair_Code_By_DARK_SURYA = Maher_Zubair({
+    
+    async function SURYA_X_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let Pair_Code_By_DARK_SURYA = DARK_SURYA({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
                 },
                 printQRInTerminal: false,
                 logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
                 browser: Browsers.macOS('Chrome')
-             });
-             if(!Pair_Code_By_DARK_SURYA.authState.creds.registered) {
+            });
+
+            if (!Pair_Code_By_DARK_SURYA.authState.creds.registered) {
                 await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_DARK_SURYA.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Pair_Code_By_DARK_SURYA.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+
+                    
+                }
+            }
+           
+            
+            
             Pair_Code_By_DARK_SURYA.ev.on('creds.update', saveCreds)
             Pair_Code_By_DARK_SURYA.ev.on("connection.update", async (s) => {
                 const {
